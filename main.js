dd9f31d6b0845534e281dfc9701221e87c469863
AaryaPrakash22sep <88137678+AaryaPrakash22sep@users.noreply.github.com>
2022-01-02 13:18:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("HI I AM AARYA PRAKASH");
+var first_name= "Akshay";
+console.log(first_name);
+var last_name="kumar";
+console.log (last_name);
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var a= 4;
+var b= 2;
+var sum= a+b;
+console.log(sum);
+
+
